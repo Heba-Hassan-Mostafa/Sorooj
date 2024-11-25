f029fbad0722fcc1dab0da03d297d008efa8d0a5
Heba-Hassan-Mostafa <hebahassan3093@gmail.com>
2024-11-26 01:18:57 +0200

    course resource

--- routes/api-v1.php
+++ routes/api-v1.php
@@ -40,8 +40,8 @@ Route::prefix("auth")->group(function () {
 
             Route::get('categories', [CategoryController::class, 'index'])->name('category.index');
            // Route::apiResource('courses', CourseController::class);
-            Route::get('courses', [CourseController::class, 'index'])->name('index');
-            Route::get('courses/{slug}', [CourseController::class, 'show'])->name('show');
+            Route::get('courses', [CourseController::class, 'index'])->name('courses.index');
+            Route::get('courses/{slug}', [CourseController::class, 'show'])->name('courses.show');
 
             Route::get('suggested-courses', [CourseController::class, 'suggestedCourses'])->name('suggested-courses');
 
